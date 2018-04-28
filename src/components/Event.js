a1eac5bef809e8ea72e980774957e1bbde83d8e5
scott <you@example.comitsmescottb123@gmail.com>
2018-04-28 16:44:41 -0400

    rendering event with correct time format

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 const Event = (props) => {
 
+  const parseTime = (dateTime) => {
+    let newDate = new Date(dateTime)
+    let getHours = newDate.getUTCHours() > 12 ? newDate.getUTCHours() - 12 : newDate.getUTCHours()
+    let getMinutes = String(newDate.getUTCMinutes())
+    getMinutes === '0' ? getMinutes += '0' : getMinutes
+    let newTime = getHours + ':' + getMinutes
+    newDate.getUTCHours() > 12 ? newTime += ' PM' : newTime += ' AM'
+    return newTime
+
+
+  }
   return (
     <li>Name: {props.event.name} |
         Start Date: {props.event.date_start} |
         End Date: {props.event.date_end} |
-        Start Time: {props.event.time_start !== null ? new Date(props.event.time_start).getUTCHours() : 'No Time Given'} | 
-        End Time: {props.event.time_end} |
+        Start Time: {props.event.time_start !== null ? parseTime(props.event.time_start) : 'No Time Given'} |
+        End Time: {props.event.time_end !== null ? parseTime(props.event.time_end) : 'No Time Given'}  |
         Votes: {props.event.votes}
     </li>
   )
